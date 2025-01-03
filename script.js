738bbb589d8882121254af7de020ae2a8968a88a
Kai_TartNenas <167038199+TartNenas@users.noreply.github.com>
2025-01-03 14:51:18 +0800

    format on save

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 // Carousel Dragging Functionality (Desktop)
 const carousel = document.querySelector(".review-carousel");
 
-let isDragging = false, startX, startScrollLeft;
+let isDragging = false,
+  startX,
+  startScrollLeft;
 
 const dragStart = (e) => {
-    isDragging = true;
-    carousel.classList.add("dragging");
-    startX = e.pageX || e.touches[0].pageX;
-    startScrollLeft = carousel.scrollLeft;
-}
+  isDragging = true;
+  carousel.classList.add("dragging");
+  startX = e.pageX || e.touches[0].pageX;
+  startScrollLeft = carousel.scrollLeft;
+};
 
 const dragging = (e) => {
-    if (!isDragging) return;
-    e.preventDefault();
-    const x = e.pageX || e.touches[0].pageX;
-    carousel.scrollLeft = startScrollLeft - (x - startX);
-}
+  if (!isDragging) return;
+  e.preventDefault();
+  const x = e.pageX || e.touches[0].pageX;
+  carousel.scrollLeft = startScrollLeft - (x - startX);
+};
 
 const dragStop = () => {
-    isDragging = false;
-    carousel.classList.remove("dragging");
-}
+  isDragging = false;
+  carousel.classList.remove("dragging");
+};
 
 // Mouse Events
 carousel.addEventListener("mousedown", dragStart);
@@ -33,8 +35,8 @@ carousel.addEventListener("touchmove", dragging);
 carousel.addEventListener("touchend", dragStop);
 
 // Navbar Button Event
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.navbar-btn').addEventListener('click', function() {
-        window.location.href = 'contactus.php';
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".navbar-btn").addEventListener("click", function () {
+    window.location.href = "contactus.php";
+  });
 });
